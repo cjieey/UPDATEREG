6c6fb4146758a4a25b68d3fbb60f0bc794692bd3
C3jieey <105692921+cjieey@users.noreply.github.com>
2023-12-11 15:04:00 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/guilogin/GUILOGIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guilogin;
+
+/**
+ *
+ * @author GAMER
+ */
+public class GUILOGIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
